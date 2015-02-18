f66ad5e0d14d648986ad8bf5b6c4367c9ebb5254
johnnay <johnnay@thelonious.cas.vanderbilt.edu>
2015-02-17 20:32:41 -0600

    description and .r file with package info too, and some more documentation

---
+++ R/fsm.R
@@ -0,0 +1,10 @@
+#' fsm: A package for estimating FSM models.
+#'
+#' The fsm package provides one primary function for estimating a fsm decision
+#' model and helper functions.
+#'
+#' @section FSM functions: The fsm functions ...
+#'
+#' @docType package
+#' @name fsm
+NULL
