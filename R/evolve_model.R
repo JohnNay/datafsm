2776cdd772275515e7d4c5eae8456b62ee098613
johnnay <johnnay@thelonious.cas.vanderbilt.edu>
2015-03-09 12:05:03 -0500

    citation related things

--- R/evolve_model.R
+++ R/evolve_model.R
@@ -90,20 +90,7 @@
 #' cdata <- data.frame(period = 1:5, outcome = c(1,2,1,1,1),
 #' my.decision1 = c(1,0,1,1,1), other.decision1 = c(0,0,0,1,1))
 #' (result <- evolve_model(cdata))
-#'
-#' # Use data from this package:
-#' data(ipd_data)
-#' percent_train <- 0.80
-#' ipd_samples <- nrow(ipd_data)
-#' ntrain <- as.integer(percent_train * ipd_samples)
-#' # Back up to just before a new interaction starts:
-#' ntrain <- ntrain - ipd_data[ntrain,"period"]
-#' train_indices <- 1:ntrain
-#' test_indices <- 1:ipd_samples
-#' test_indices <- test_indices[! test_indices %in% train_indices]
-#' train_data <- ipd_data[train_indices, ]
-#' test_data <- ipd_data[test_indices,]
-#' summary(evolve_model(train_data, test_data, run = 5))
+#' summary(result)
 #' @export
 
 ################################################################################
