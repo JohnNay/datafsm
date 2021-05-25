eba22265f02bedc61269fd83047ef7698f1825d3
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2021-05-25 14:26:19 -0500

    Try another approach to vignette figures.

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -2,4 +2,13 @@
 # Must manually move image files from datafsm/ to datafsm/vignettes/ after knit
 
 library(knitr)
-knit("vignettes/FRD_vignette.Rmd.orig", "vignettes/FRD_vignette.Rmd")
+prebuild_vignettes <- function() {
+  old_wd <- getwd()
+  if (! basename(getwd()) == "vignettes" && dir.exists("./vignettes")) {
+    setwd("vignettes")
+    on.exit(setwd(old_wd))
+  }
+  knit("FRD_vignette.Rmd.orig", "FRD_vignette.Rmd")
+}
+
+prebuild_vignettes()
